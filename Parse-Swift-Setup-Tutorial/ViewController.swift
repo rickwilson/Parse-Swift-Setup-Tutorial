0d261ae45c1f3feaa94e1d0cef41537ac48fafb8
Rick Wilson <rick@teamwilson.org>
2015-04-14 10:01:10 -0600

    Added test Parse code for tutorial...

--- Parse-Swift-Setup-Tutorial/ViewController.swift
+++ Parse-Swift-Setup-Tutorial/ViewController.swift
@@ -7,17 +7,28 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        testDataToParse()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func testDataToParse() {
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            println("Object has been saved.")
+        }
     }
 
 
